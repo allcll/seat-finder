008fcf6c3d36c2b89c3fa27739e5b3a6dcb88707
3Juhwan <13selfesteem91@naver.com>
2025-01-16 15:53:13 +0900

    init: 프로젝트 세팅

---
+++ src/test/java/kr/allcll/seatfinder/SeatFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.allcll.seatfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
