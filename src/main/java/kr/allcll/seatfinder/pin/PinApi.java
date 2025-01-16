6f56cd7661788b35a4bc883589823ac0fc205803
3Juhwan <13selfesteem91@naver.com>
2025-01-16 16:06:57 +0900

    feat: api 뼈대 코드 작성

---
+++ src/main/java/kr/allcll/seatfinder/pin/PinApi.java
@@ -0,0 +1,8 @@
+package kr.allcll.seatfinder.pin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PinApi {
+
+}
---
+++ src/main/java/kr/allcll/seatfinder/subject/SubjectApi.java
@@ -0,0 +1,9 @@
+package kr.allcll.seatfinder.subject;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubjectApi {
+
+    
+}
