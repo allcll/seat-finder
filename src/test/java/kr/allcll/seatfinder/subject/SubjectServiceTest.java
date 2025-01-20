1490b75cf507ca95d873b707989ab858ef4ef907
3Juhwan <13selfesteem91@naver.com>
2025-01-20 23:36:12 +0900

    refactor: 테스트 클래스명 수정

--- src/test/java/kr/allcll/seatfinder/subject/SubjectServiceTest.java
+++ src/test/java/kr/allcll/seatfinder/subject/SubjectServiceTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class SubjectAppTest {
+class SubjectServiceTest {
 
     @Autowired
     private SubjectService subjectService;
