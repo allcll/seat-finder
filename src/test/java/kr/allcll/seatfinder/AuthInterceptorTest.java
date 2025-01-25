c1e50f4b78df8919df193ef62baa91874ed216d4
Sumin <new.sumyang@gmail.com>
2025-01-25 19:39:02 +0900

    refactor: 토큰 검증 테스트 클래스 수정

--- src/test/java/kr/allcll/seatfinder/AuthInterceptorTest.java
+++ src/test/java/kr/allcll/seatfinder/AuthInterceptorTest.java
@@ -1,8 +1,10 @@
-package kr.allcll.seatfinder.pin;
+package kr.allcll.seatfinder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import kr.allcll.seatfinder.pin.PinApi;
+import kr.allcll.seatfinder.pin.PinService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,10 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(PinApi.class)
-class PinApiTest {
+class AuthInterceptorTest {
+
+    private static final String TOKEN_KEY = "token";
+    private static final String TOKEN_VALIDATE_API_URL = "/api/pin?subjectId=1";
 
     @Autowired
     private MockMvc mockMvc;
@@ -20,12 +25,10 @@ class PinApiTest {
     @MockitoBean
     private PinService pinService;
 
-    private static final String TOKEN_KEY = "token";
-
     @Test
     @DisplayName("쿠키에 토큰이 존재하지 않을 때 토큰의 생성을 확인한다.")
     void addPinOnSubject() throws Exception {
-        MockHttpServletResponse response = mockMvc.perform(post("/api/pin?subjectId=1"))
+        MockHttpServletResponse response = mockMvc.perform(post(TOKEN_VALIDATE_API_URL))
             .andReturn()
             .getResponse();
         assertThat(response.getCookie(TOKEN_KEY)).isNotNull();
