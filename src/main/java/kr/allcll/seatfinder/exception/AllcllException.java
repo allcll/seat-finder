a27e63000e99676bc48c1f7f00a955d4ca7f57a6
Sumin <new.sumyang@gmail.com>
2025-01-27 17:40:18 +0900

    feat: 공통 예외 코드 작성

---
+++ src/main/java/kr/allcll/seatfinder/exception/AllcllException.java
@@ -0,0 +1,24 @@
+package kr.allcll.seatfinder.exception;
+
+public class AllcllException extends RuntimeException {
+
+    private final String errorCode;
+
+    public AllcllException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public static AllcllException from(ExceptionMessage e) {
+        return new AllcllException(e.getMessage(), e.name());
+    }
+
+    public static AllcllException of(int number, ExceptionMessage e) {
+        String exceptionMessage = String.format(e.getMessage(), number);
+        return new AllcllException(exceptionMessage, e.name());
+    }
+}
