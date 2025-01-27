f00130cc2a574aaf17c902e8828040b0f7115b67
Sumin <new.sumyang@gmail.com>
2025-01-27 16:17:13 +0900

    refactor: entityManager 삭제, `@Transactional` 삭제

--- src/test/java/kr/allcll/seatfinder/pin/PinServiceTest.java
+++ src/test/java/kr/allcll/seatfinder/pin/PinServiceTest.java
@@ -3,7 +3,6 @@ package kr.allcll.seatfinder.pin;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import jakarta.persistence.EntityManager;
 import java.util.List;
 import kr.allcll.seatfinder.subject.Subject;
 import kr.allcll.seatfinder.subject.SubjectRepository;
@@ -19,8 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 class PinServiceTest {
 
     private static final int MAX_PIN_NUMBER = 5;
-    private static final String NOT_REACH_MAX_TOKEN = "tokenIdNotReachMax";
-    private static final String REACH_MAX_TOKEN = "tokenIdReachMax";
     private static final String PIN_REPOSITORY_FIND_ERROR_MESSAGE = "핀에 등록된 과목이 아닙니다.";
     private static final String EXIST_PIN_ERROR_MESSAGE = "이미 핀 등록된 과목 입니다.";
     private static final String OVER_PIN_COUNT_ERROR_MESSAGE = String.format("이미 %d개의 핀을 등록했습니다.", MAX_PIN_NUMBER);
@@ -34,28 +31,25 @@ class PinServiceTest {
     @Autowired
     private SubjectRepository subjectRepository;
 
-    @Autowired
-    private EntityManager entityManager;
-
-    private List<Subject> subjects;
+    private static final String TOKEN = "token";
 
     @BeforeEach
+    @Transactional
     void setUp() {
         pinRepository.deleteAllInBatch();
         subjectRepository.deleteAllInBatch();
-        entityManager.createNativeQuery("ALTER TABLE subject ALTER COLUMN id RESTART WITH 1").executeUpdate();
-        subjects = saveSixSubject();
     }
 
     @Test
     @DisplayName("핀이 5개 미만일 경우 정상 등록을 검증한다.")
-    @Transactional
     void addPinOnSubject() {
         // given
-        pinService.addPinOnSubject(1L, NOT_REACH_MAX_TOKEN);
+        Subject subjectA = createSubject("컴퓨터구조", "003278", "001", "김보예");
+        subjectRepository.save(subjectA);
+        pinService.addPinOnSubject(subjectA.getId(), TOKEN);
 
         // when
-        List<Pin> result = pinRepository.findAllByToken(NOT_REACH_MAX_TOKEN);
+        List<Pin> result = pinRepository.findAllByToken(TOKEN);
 
         // then
         assertThat(result).hasSize(1);
@@ -63,13 +57,26 @@ class PinServiceTest {
 
     @Test
     @DisplayName("핀이 5개 이상일 경우 예외를 검증한다.")
-    @Transactional
     void canNotAddPinOnSubject() {
         // given
-        saveFivePinToMaxToken(subjects);
+        Subject subjectA = createSubject("컴퓨터구조", "003278", "001", "김보예");
+        Subject subjectB = createSubject("운영체제", "003279", "001", "김수민");
+        Subject subjectC = createSubject("자료구조", "003280", "001", "김봉케");
+        Subject subjectD = createSubject("알고리즘", "003281", "001", "오현지");
+        Subject subjectE = createSubject("컴퓨터구조", "003278", "002", "전유채");
+        Subject overCountSubject = createSubject("컴퓨터구조", "003278", "002", "전유채");
+        subjectRepository.saveAll(List.of(subjectA, subjectB, subjectC, subjectD, subjectE, overCountSubject));
+        pinRepository.saveAll(
+            List.of(
+                new Pin(TOKEN, subjectA),
+                new Pin(TOKEN, subjectB),
+                new Pin(TOKEN, subjectC),
+                new Pin(TOKEN, subjectD),
+                new Pin(TOKEN, subjectE))
+        );
 
-        // then
-        assertThatThrownBy(() -> pinService.addPinOnSubject(6L, REACH_MAX_TOKEN))
+        // when, then
+        assertThatThrownBy(() -> pinService.addPinOnSubject(overCountSubject.getId(), TOKEN))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(OVER_PIN_COUNT_ERROR_MESSAGE);
     }
@@ -77,81 +84,60 @@ class PinServiceTest {
 
     @Test
     @DisplayName("이미 핀 등록된 과목일 경우 예외를 검증한다.")
-    @Transactional
     void alreadyExistPinSubject() {
         // given
-        pinService.addPinOnSubject(1L, NOT_REACH_MAX_TOKEN);
+        Subject subject = createSubject("컴퓨터구조", "003278", "001", "김보예");
+        subjectRepository.save(subject);
+        pinRepository.save(new Pin(TOKEN, subject));
 
-        // then
-        assertThatThrownBy(() -> pinService.addPinOnSubject(1L, NOT_REACH_MAX_TOKEN))
+        // when, then
+        assertThatThrownBy(() -> pinService.addPinOnSubject(subject.getId(), TOKEN))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(EXIST_PIN_ERROR_MESSAGE);
     }
 
     @Test
     @DisplayName("핀의 삭제를 검증한다.")
-    @Transactional
     void deletePin() {
         // given
-        saveFivePinToMaxToken(subjects);
+        Subject subject = createSubject("컴퓨터구조", "003278", "001", "김보예");
+        subjectRepository.save(subject);
+        pinRepository.save(new Pin(TOKEN, subject));
 
         // when
-        pinService.deletePinOnSubject(1L, REACH_MAX_TOKEN);
+        pinService.deletePinOnSubject(subject.getId(), TOKEN);
 
         // then
-        assertThat(pinRepository.findAllByToken(REACH_MAX_TOKEN)).hasSize(4);
+        assertThat(pinRepository.findAllByToken(TOKEN)).hasSize(0);
     }
 
     @Test
     @DisplayName("등록되지 않은 핀의 삭제에 대한 예외를 검증한다.")
-    @Transactional
     void deleteNotExistPin() {
         // given
-        saveFivePinToMaxToken(subjects);
+        String token = "token";
+        Subject pinnedSubject = createSubject("핀과목", "123456", "001", "김보예");
+        Subject notPinnedSubject = createSubject("핀 안된 과목", "654321", "001", "김주환");
+        subjectRepository.saveAll(List.of(pinnedSubject, notPinnedSubject));
+        pinRepository.save(new Pin(token, pinnedSubject));
 
         // when, then
-        assertThatThrownBy(() -> pinService.deletePinOnSubject(6L, REACH_MAX_TOKEN))
+        assertThatThrownBy(() -> pinService.deletePinOnSubject(notPinnedSubject.getId(), token))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(PIN_REPOSITORY_FIND_ERROR_MESSAGE);
     }
 
     @Test
     @DisplayName("존재하지 않는 토큰에 대한 예외를 검증한다.")
-    @Transactional
     void deleteNotExistToken() {
+        // given
+        Subject subject = createSubject("컴퓨터구조", "003278", "001", "김보예");
+        subjectRepository.save(subject);
+
         // when, then
-        assertThatThrownBy(() -> pinService.deletePinOnSubject(1L, NOT_REACH_MAX_TOKEN))
+        assertThatThrownBy(() -> pinService.deletePinOnSubject(subject.getId(), TOKEN))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(PIN_REPOSITORY_FIND_ERROR_MESSAGE);
-    }
-
-    private void saveFivePinToMaxToken(List<Subject> subjects) {
-        pinRepository.saveAll(
-            List.of(
-                new Pin(REACH_MAX_TOKEN, subjects.get(0)),
-                new Pin(REACH_MAX_TOKEN, subjects.get(1)),
-                new Pin(REACH_MAX_TOKEN, subjects.get(2)),
-                new Pin(REACH_MAX_TOKEN, subjects.get(3)),
-                new Pin(REACH_MAX_TOKEN, subjects.get(4)))
-        );
-    }
-
-    private List<Subject> saveSixSubject() {
-        Subject subjectA = createSubject("컴퓨터구조", "003278", "001", "김보예");
-        Subject subjectB = createSubject("운영체제", "003279", "001", "김수민");
-        Subject subjectC = createSubject("자료구조", "003280", "001", "김봉케");
-        Subject subjectD = createSubject("알고리즘", "003281", "001", "오현지");
-        Subject subjectE = createSubject("컴퓨터구조", "003278", "002", "전유채");
-        Subject subjectF = createSubject("컴퓨터구조", "003278", "003", "김주환");
-        return subjectRepository.saveAll(
-            List.of(
-                subjectA,
-                subjectB,
-                subjectC,
-                subjectD,
-                subjectE,
-                subjectF)
-        );
     }
 
     private Subject createSubject(
