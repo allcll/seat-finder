2cd1c69bec639cf8950ee15e997c0f52b24031bf
Sumin <new.sumyang@gmail.com>
2025-01-27 17:40:44 +0900

    feat: 에러 코드 클래스 추가

---
+++ src/main/java/kr/allcll/seatfinder/exception/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package kr.allcll.seatfinder.exception;
+
+public enum ExceptionMessage {
+
+    MAX_PIN_EXCEPTION("이미 %d개의 핀을 등록했습니다."),
+    EXIST_PIN_EXCEPTION("이미 핀 등록된 과목입니다."),
+    PIN_AND_SUBJECT_NOT_MATCH("핀에 등록된 과목이 아닙니다."),
+    NOT_EXIST_SUBJECT("존재하지 않는 과목 입니다.");
+
+    private String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
