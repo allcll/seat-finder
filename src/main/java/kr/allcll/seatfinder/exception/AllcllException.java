26602047fc19765b9d40379a9ad80f068014f5b8
Sumin <new.sumyang@gmail.com>
2025-01-28 18:21:24 +0900

    refactor: Throwable 포함한 생성자 추가

--- src/main/java/kr/allcll/seatfinder/exception/AllcllException.java
+++ src/main/java/kr/allcll/seatfinder/exception/AllcllException.java
@@ -7,13 +7,18 @@ public class AllcllException extends RuntimeException {
 
     private final String errorCode;
 
-    public AllcllException(AllcllErrorCode e) {
-        super(e.getMessage());
-        this.errorCode = e.name();
+    public AllcllException(AllcllErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode.name();
     }
 
-    public AllcllException(AllcllErrorCode e, Object... args) {
-        super(String.format(e.getMessage(), args));
-        this.errorCode = e.name();
+    public AllcllException(String message, Throwable cause, AllcllErrorCode errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode.name();
+    }
+
+    public AllcllException(AllcllErrorCode errorCode, Object... args) {
+        super(String.format(errorCode.getMessage(), args));
+        this.errorCode = errorCode.name();
     }
 }
