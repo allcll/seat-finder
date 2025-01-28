8c09f826d5d2ccdcc29bcff158f9a4c2a90a1b57
3Juhwan <13selfesteem91@naver.com>
2025-01-29 01:39:17 +0900

    feat: LocalDateTime 역직렬화 포맷 변경

--- src/main/java/kr/allcll/seatfinder/sse/SseEventBuilderFactory.java
+++ src/main/java/kr/allcll/seatfinder/sse/SseEventBuilderFactory.java
@@ -2,6 +2,8 @@ package kr.allcll.seatfinder.sse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
@@ -11,6 +13,11 @@ public class SseEventBuilderFactory {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private static final long RECONNECT_TIME_MILLIS = 1000L;
+
+    static {
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     public static SseEventBuilder create(String eventName, Object value) {
         try {
