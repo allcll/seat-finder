0586cb71ca0f17837f78802070caf003dfcdf4f7
3Juhwan <13selfesteem91@naver.com>
2025-01-28 17:41:57 +0900

    test: 여러 클라이언트 SSE 연결한 상황 테스트

--- src/test/java/kr/allcll/seatfinder/sse/SseIntegrationTest.java
+++ src/test/java/kr/allcll/seatfinder/sse/SseIntegrationTest.java
@@ -1,8 +1,8 @@
 package kr.allcll.seatfinder.sse;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
+import groovy.util.logging.Slf4j;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import java.io.BufferedReader;
@@ -13,18 +13,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.test.context.TestPropertySource;
 
+@Slf4j
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "app.scheduling.enabled=false") // 테스트에서 스케줄러 비활성화
 class SseIntegrationTest {
 
     private static final Logger log = LoggerFactory.getLogger(SseIntegrationTest.class);
 
-    @MockitoSpyBean
+    @Autowired
     private SseService sseService;
 
     @LocalServerPort
@@ -38,9 +40,6 @@ class SseIntegrationTest {
     @DisplayName("SSE를 연결하고, 최초 메시지를 받는다.")
     @Test
     void sseConnectionTest() {
-        long mockSseEmitterTimeout = 500L;
-        when(sseService.createSseEmitter()).thenReturn(new SseEmitter(mockSseEmitterTimeout));
-
         // when
         Response response = RestAssured.given()
             .accept("text/event-stream")
@@ -55,30 +54,81 @@ class SseIntegrationTest {
         SseTestHelper.assertResponseContainsMessage(response, "success");
     }
 
+    @DisplayName("동시에 여러 클라이언트와 SSE 연결을 유지하고, 모두에게 메시지를 전달한다.")
+    @Test
+    void ssePropagationTest() {
+        // given
+        Response response1 = RestAssured.given()
+            .accept("text/event-stream")
+            .when()
+            .get("/api/connect")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+
+        Response response2 = RestAssured.given()
+            .accept("text/event-stream")
+            .when()
+            .get("/api/connect")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+
+        // when
+        sseService.propagate("message", "Hello, SSE!");
+
+        // then
+        SseTestHelper.assertResponseIsEqualsToMessage(response1, "retry:1000\n"
+            + "event:connection\n"
+            + "data:success\n"
+            + "\n"
+            + "retry:1000\n"
+            + "event:message\n"
+            + "data:\"Hello, SSE!\"\n"
+            + "\n");
+        SseTestHelper.assertResponseIsEqualsToMessage(response2, "retry:1000\n"
+            + "event:connection\n"
+            + "data:success\n"
+            + "\n"
+            + "retry:1000\n"
+            + "event:message\n"
+            + "data:\"Hello, SSE!\"\n"
+            + "\n");
+    }
+
     private static class SseTestHelper {
 
         private static final long TIMEOUT = 1000;
 
-        public static void assertResponseContainsMessage(Response response, String message) {
+        public static void assertResponseIsEqualsToMessage(Response response, String message) {
             String body = response.getBody().asString();
             try (BufferedReader reader = new BufferedReader(new StringReader(body))) {
-                String eventReceived = readResponse(reader, message);
-                assertThat(eventReceived).contains(message);
+                String eventReceived = readResponse(reader);
+                assertThat(eventReceived).isEqualTo(message);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
 
-        private static String readResponse(BufferedReader reader, String message) throws IOException {
+        public static void assertResponseContainsMessage(Response response, String... messages) {
+            String body = response.getBody().asString();
+            try (BufferedReader reader = new BufferedReader(new StringReader(body))) {
+                String eventReceived = readResponse(reader);
+                assertThat(eventReceived).contains(messages);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static String readResponse(BufferedReader reader) throws IOException {
             StringBuilder lines = new StringBuilder();
             String line;
             long startTime = System.currentTimeMillis();
             while ((line = reader.readLine()) != null) {
                 log.info("Received: {}", line);
                 lines.append(line).append("\n");
-                if (line.startsWith(message)) {
-                    break;
-                }
                 if (System.currentTimeMillis() - startTime > TIMEOUT) {
                     break;
                 }
