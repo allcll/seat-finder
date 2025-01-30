c69f7f3611e80eff5a21a7790875dcf60b190f22
Sumin <new.sumyang@gmail.com>
2025-01-31 01:59:24 +0900

    refactor: 쿼리 수정

--- src/main/java/kr/allcll/seatfinder/cart/CartRepository.java
+++ src/main/java/kr/allcll/seatfinder/cart/CartRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    @Query("SELECT c FROM Cart c ORDER BY c.queryTime DESC")
+    @Query(value = "SELECT c FROM Cart c ORDER BY c.queryTime DESC LIMIT 2411", nativeQuery = true)
     List<Cart> findTop2411ByOrderByQueryTimeDesc();
 }
