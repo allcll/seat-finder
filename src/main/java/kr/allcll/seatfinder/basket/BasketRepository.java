9a0c2f72d0ed4ab6ddccc40dda037dc014ff2812
Sumin <new.sumyang@gmail.com>
2025-02-02 21:15:09 +0900

    feat: repository 레이어 기능 추가

--- src/main/java/kr/allcll/seatfinder/basket/BasketRepository.java
+++ src/main/java/kr/allcll/seatfinder/basket/BasketRepository.java
@@ -1,7 +1,9 @@
 package kr.allcll.seatfinder.basket;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BasketRepository extends JpaRepository<Basket, Long> {
 
+    List<Basket> findBySubjectId(Long id);
 }
