b5832afdd9a31df56f074fdc62160ff105ddcab8
Sumin <new.sumyang@gmail.com>
2025-02-02 21:13:33 +0900

    feat: DepartmentRegisters 정팩메 추가

--- src/main/java/kr/allcll/seatfinder/basket/dto/DepartmentRegisters.java
+++ src/main/java/kr/allcll/seatfinder/basket/dto/DepartmentRegisters.java
@@ -1,8 +1,16 @@
 package kr.allcll.seatfinder.basket.dto;
 
+import java.util.List;
+import kr.allcll.seatfinder.basket.Basket;
+
 public record DepartmentRegisters(
     String registerDepartment,
     Integer eachCount
 ) {
 
+    public static List<DepartmentRegisters> from(List<Basket> baskets) {
+        return baskets.stream()
+            .map(basket -> new DepartmentRegisters(basket.getStudentDivNm(), basket.getRcnt()))
+            .toList();
+    }
 }
