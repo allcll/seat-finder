3b7cdaf9bccaab81d232f851db59ba7510933ae0
3Juhwan <13selfesteem91@naver.com>
2025-02-07 17:08:43 +0900

    docs: 테스트 픽스처 주석 추가

--- src/test/java/kr/allcll/seatfinder/support/fixture/BasketFixture.java
+++ src/test/java/kr/allcll/seatfinder/support/fixture/BasketFixture.java
@@ -5,6 +5,11 @@ import kr.allcll.seatfinder.subject.Subject;
 
 public class BasketFixture {
 
+    /*
+        빈 Basket은 아래와 같이 생성됩니다.
+        0과 null로 초기화된 필드는 수정하면 안됩니다.
+        위 기준은 실제 API 요청으로 만들었습니다.
+     */
     public static Basket createEmptyBasket(Subject subject) {
         return new Basket(subject, null, null, "", "", null, null,
             null, null, null, null, 99, 0, 0,
