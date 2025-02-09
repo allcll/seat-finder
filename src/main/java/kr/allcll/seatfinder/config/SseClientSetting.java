e0c5f1a216ac9c7c10a0db0361ab34dd0381e22b
Sumin <new.sumyang@gmail.com>
2025-02-10 03:04:51 +0900

    refactor: 클래스 네이밍 변경

--- src/main/java/kr/allcll/seatfinder/config/SseClientSetting.java
+++ src/main/java/kr/allcll/seatfinder/config/SseClientSetting.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class SseClientConfig {
+public class SseClientSetting {
 
     private final SseClientService sseClientService;
 
