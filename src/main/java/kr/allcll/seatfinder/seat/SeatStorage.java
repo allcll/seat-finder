06a2f41ac6fd4ed980d1cab5ba8a1c2f9ae9e4c3
Sumin <new.sumyang@gmail.com>
2025-02-10 02:45:59 +0900

    refactor: SeatStorage 자료 구조 변경

--- src/main/java/kr/allcll/seatfinder/seat/SeatStorage.java
+++ src/main/java/kr/allcll/seatfinder/seat/SeatStorage.java
@@ -1,21 +1,25 @@
 package kr.allcll.seatfinder.seat;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import kr.allcll.seatfinder.subject.Subject;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SeatStorage {
 
-    private final List<Seat> seats;
+    private final Map<Long, Seat> seats;
 
     public SeatStorage() {
-        this.seats = new ArrayList<>();
+        this.seats = new ConcurrentHashMap<>();
     }
 
     public List<Seat> getNonMajorSeats(int limit) {
-        return seats.stream()
+        Collection<Seat> seatsValue = seats.values();
+        return seatsValue.stream()
             .filter(seat -> seat.getSubject().isNonMajor())
             .sorted((s1, s2) -> s2.getSeatCount() - s1.getSeatCount())
             .limit(limit)
@@ -25,7 +29,7 @@ public class SeatStorage {
     public List<Seat> getSeats(List<Subject> subjects) {
         List<Seat> result = new ArrayList<>();
         for (Subject subject : subjects) {
-            for (Seat seat : seats) {
+            for (Seat seat : seats.values()) {
                 if (seat.getSubject().getId().equals(subject.getId())) {
                     result.add(seat);
                     break;
@@ -36,10 +40,12 @@ public class SeatStorage {
     }
 
     public void add(Seat seat) {
-        seats.add(seat);
+        seats.put(seat.getSubject().getId(), seat);
     }
 
     public void addAll(List<Seat> seats) {
-        this.seats.addAll(seats);
+        for (Seat seat : seats) {
+            this.seats.put(seat.getSubject().getId(), seat);
+        }
     }
 }
