6ce72ffb1ed44f8e0e2fc9d0af2bcb76977cbd8e
3Juhwan <13selfesteem91@naver.com>
2025-02-11 02:06:06 +0900

    refactor: 크롤러 API 클래스 수정

--- src/main/java/kr/allcll/seatfinder/crawler/CrawlerApi.java
+++ src/main/java/kr/allcll/seatfinder/crawler/CrawlerApi.java
@@ -1,27 +1,48 @@
 package kr.allcll.seatfinder.crawler;
 
+import kr.allcll.seatfinder.external.CrawlerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
 
-@RestController
+@Slf4j
+@Component
 @RequiredArgsConstructor
 public class CrawlerApi {
 
     private final CrawlerService crawlerService;
 
     // 교양 top20을 자료구조에 저장한다.
-    @PostMapping("/api/save-nonmajors")
-    public ResponseEntity<Void> saveNonMajorSubjects() {
-        crawlerService.saveNonMajorSubjects();
-        return ResponseEntity.ok().build();
+    @Retryable(
+        retryFor = {Exception.class},
+        maxAttempts = 1000,
+        backoff = @Backoff(delay = 1000)
+    )
+    public void saveNonMajorSubjects() {
+        try {
+            crawlerService.saveNonMajorSubjects();
+        } catch (Exception e) {
+            log.error("교양 top20 저장 중 오류 발생", e);
+            throw e;
+        }
+        log.info("교양 top20 저장 완료");
     }
 
     // 교양 top20을 크롤러에게 전달해둘 아이이다. 힌 번만 실행하면 된다.
-    @PostMapping("/api/send-nonmajor")
-    public ResponseEntity<Void> retrieveSendNonMajor() {
-        crawlerService.sendToCrawlerNonMajorRequest();
-        return ResponseEntity.ok().build();
+    @Retryable(
+        retryFor = {Exception.class},
+        maxAttempts = 1000,
+        backoff = @Backoff(delay = 5000)
+    )
+    public void retrieveSendNonMajor() {
+        try {
+            crawlerService.sendToCrawlerNonMajorRequest();
+        } catch (Exception e) {
+            log.error("교양 top20 전달 중 오류 발생", e);
+            throw e;
+        }
+        log.info("교양 top20 전달 완료");
     }
 }
