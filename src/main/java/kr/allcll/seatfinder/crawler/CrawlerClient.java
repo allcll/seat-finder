88fa017e0772c5402be95d788574326e0c71c0f4
3Juhwan <13selfesteem91@naver.com>
2025-02-10 21:33:51 +0900

    refactor: 외부 서버 설정 분리

--- src/main/java/kr/allcll/seatfinder/crawler/CrawlerClient.java
+++ src/main/java/kr/allcll/seatfinder/crawler/CrawlerClient.java
@@ -4,6 +4,7 @@ import kr.allcll.seatfinder.crawler.dto.NonMajorRequest;
 import kr.allcll.seatfinder.crawler.dto.PinSubjectsRequest;
 import kr.allcll.seatfinder.sse.SseClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,12 +16,13 @@ public class CrawlerClient {
 
     private final RestClient restClient;
     private final SseClientService sseClientService;
-    private static final String URI = "/api/";
+    @Value("${external.host}")
+    private String host;
 
     // TODO: 어떤식으로 응답이 오는지, 어떤 HTTP method로 받는지에 따라 수정 필요. 응답으로 SUCCESS 보내줬음 좋겠음.
     public ResponseEntity<String> retrieveNonMajor(NonMajorRequest request) {
         return restClient.post()
-            .uri(URI)
+            .uri(host)
             .body(request)
             .retrieve()
             .toEntity(String.class);
@@ -28,7 +30,7 @@ public class CrawlerClient {
 
     public ResponseEntity<String> retrieveWantPinSubjectsRequest(PinSubjectsRequest request) {
         return restClient.post()
-            .uri(URI)
+            .uri(host)
             .body(request)
             .retrieve()
             .toEntity(String.class);
@@ -38,7 +40,7 @@ public class CrawlerClient {
     @Scheduled(fixedDelay = 1000 * 60)
     public void requestSseConnection() {
         restClient.post()
-            .uri(URI)
+            .uri(host)
             .retrieve()
             .toEntity(String.class);
         sseClientService.getSseData();
